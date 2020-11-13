9d8cd373527a8dde977195091ac8a3b46698a234
ophir-garage <72793145+ophir-garage@users.noreply.github.com>
2020-11-13 11:51:16 +0200

    1st version

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    PixelArray.showColor(neopixel.colors(NeoPixelColors.Red))
+})
+input.onButtonPressed(Button.B, function () {
+    PixelArray.clear()
+    PixelArray.show()
+})
+let PixelArray: neopixel.Strip = null
+PixelArray = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
 basic.forever(function () {
 	
 })
